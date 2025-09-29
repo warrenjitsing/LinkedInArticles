408bf429223f5ba29d5973d66b17e2630426aad9
Warren Jitsing <warren.jitsing@infcon.co.za>
2025-09-29 19:17:56 +0200

    :wrench: config(std_lib_http_client): add release mode and LTO

--- articles/0004_std_lib_http_client/setup.sh
+++ articles/0004_std_lib_http_client/setup.sh
@@ -10,3 +10,15 @@ if [ ! -d ".venv" ]; then
         python3 -m pip install -r requirements.txt
     )
 fi
+
+# --- Build Debug Version ---
+echo "Configuring and building Debug version..."
+cmake -S . -B build_debug -DCMAKE_BUILD_TYPE=Debug
+cmake --build build_debug
+
+# --- Build Release Version ---
+echo "Configuring and building Release version..."
+cmake -S . -B build_release -DCMAKE_BUILD_TYPE=Release
+cmake --build build_release
+
+echo "Setup complete. Builds are in build_debug/ and build_release/"
--- articles/0004_std_lib_http_client/src/rust/src/main.rs
+++ articles/0004_std_lib_http_client/src/rust/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    if cfg!(debug_assertions) {
+        // This code will only be included in debug builds
+        println!("This is a debug build!");
+    } else {
+        // This code will only be included in release builds
+        println!("This is a release build!");
+    }
 }
